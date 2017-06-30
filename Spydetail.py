a9871c2829a626bc07adb9b4de85f460db1520c4
Jyoti <jyotithakur15111@gmail.com>
2017-06-30 16:59:29 +0530

    complete my project

--- Spydetail.py
+++ Spydetail.py
@@ -3,38 +3,52 @@
 # spy_rating=4.5
 # spysalutation='Ms.'
 # spy_is_online=True
-# class spy:
-#     def __init__(self,name,salutaion,age,rating):
-#         self.name = name
-#         self.salutation = salutaion
-#         self.age = age
-#         self.rating = rating
-#         self.chat =[]
-# friend1= spy('jyoti','mr', '21', '4')
-# friend2= spy('sudiksha','mr', '21', '5')
-# friend3=spy('komal','ms','20','6')
-# friends = [friend1,friend2,friend3]
-friends=[
-    {
-        'name':'jyoti',
-        'saluatation':'ms',
-        'rating':32,
-        'age': 21,
-        'chats':[]
-     },
-    {
-        'name':'sudi',
-        'saluatation':'ms',
-        'rating':31,
-        'age': 20,
-        'chats':[]
-    }
-]
-spy = {
-    'name': 'bond',
-    'salutation': 'Mr.',
-    'age': 24,
-    'rating': 4.7,
-    'is_online': True
+from datetime import datetime
+class Spy:
+    def __init__(self,name,salutaion,age,rating):
+       self.name = name
+       self.salutation = salutaion
+       self.age = age
+       self.rating = rating
+       self.chats =[]
+       self.current_status_message=None
 
-}
+class ChatMessage:
+      def __init__(self, message, sent_by_me):
+        self.message = message
+        self.time = datetime.now()
+        self.sent_by_me = sent_by_me
+
+spy = Spy('bond', 'Mr.', 24, 4.7)
+friend1= Spy('jyoti','ms', '21', '4')
+friend2= Spy('sudiksha','ms', '21', '5')
+friend3=Spy('komal','ms','20','6')
+friends = [friend1,friend2,friend3]
+
+
+
+
+# friends=[
+#     {
+#         'name':'jyoti',
+#         'saluatation':'ms',
+#         'rating':32,
+#         'age': 21,
+#         'chats':[]
+#      },
+#     {
+#         'name':'sudi',
+#         'saluatation':'ms',
+#         'rating':31,
+#         'age': 20,
+#         'chats':[]
+#     }
+# ]
+# spy = {
+#     'name': 'bond',
+#     'salutation': 'Mr.',
+#     'age': 24,
+#     'rating': 4.7,
+#     'is_online': True
+#
+# }
