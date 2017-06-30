6efc72702198e29dd4fcc2cd275253084c65c1ca
Jyoti <jyotithakur15111@gmail.com>
2017-06-30 12:42:42 +0530

    in this file add spy detail using dictionary

---
+++ Spydetail.py
@@ -0,0 +1,40 @@
+# spy_name="jyoti"
+# spy_age=20
+# spy_rating=4.5
+# spysalutation='Ms.'
+# spy_is_online=True
+# class spy:
+#     def __init__(self,name,salutaion,age,rating):
+#         self.name = name
+#         self.salutation = salutaion
+#         self.age = age
+#         self.rating = rating
+#         self.chat =[]
+# friend1= spy('jyoti','mr', '21', '4')
+# friend2= spy('sudiksha','mr', '21', '5')
+# friend3=spy('komal','ms','20','6')
+# friends = [friend1,friend2,friend3]
+friends=[
+    {
+        'name':'jyoti',
+        'saluatation':'ms',
+        'rating':32,
+        'age': 21,
+        'chats':[]
+     },
+    {
+        'name':'sudi',
+        'saluatation':'ms',
+        'rating':31,
+        'age': 20,
+        'chats':[]
+    }
+]
+spy = {
+    'name': 'bond',
+    'salutation': 'Mr.',
+    'age': 24,
+    'rating': 4.7,
+    'is_online': True
+
+}
